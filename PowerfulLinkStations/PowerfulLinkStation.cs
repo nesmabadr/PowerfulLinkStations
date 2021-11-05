0fbfa6387e4138e0f2e47e285f58c9d78bc8a2f0
Nesma <nesmabadr@aucegypt.edu>
2021-11-05 16:40:44 +0200

    Read the point from Console and parse it

---
+++ PowerfulLinkStations/PowerfulLinkStation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace PowerfulLinkStations
+{
+    public class PowerfulLinkStation
+    {
+        public static void Main(string[] args)
+        {
+            bool retryInput;
+            string pointCoordinates;
+
+            do
+            {
+                Console.WriteLine("Please input the point coordinates in the following format: (x,y)");
+                pointCoordinates = Console.ReadLine();
+
+                if (!Regex.IsMatch(pointCoordinates, "\\(\\d+.*\\d*,\\d+.*\\d*\\)"))
+                {
+                    Console.WriteLine($"The input point {pointCoordinates} is not in the correct format. Please make sure to have the point with the following format: (x,y)");
+                    Console.WriteLine($"Do you want to enter a new value or exit the program? y or n");
+
+                    var retry = Console.ReadLine();
+                    retryInput = retry == "y" ? true : false;
+
+                    if (!retryInput)
+                    {
+                        Environment.Exit(0);
+                    }
+                }
+                else
+                {
+                    retryInput = false;
+                }
+
+            } while (retryInput);
+
+            float xCoordinate = float.Parse(pointCoordinates.Substring(1, pointCoordinates.IndexOf(',') - 1));
+            float yCoordinate = float.Parse(pointCoordinates.Substring(pointCoordinates.IndexOf(',') + 1, pointCoordinates.IndexOf(')') - pointCoordinates.IndexOf(',') - 1)); ;
+        }
+    }
+}
